71d9e2a980d28da77677dfc531ba7a7436549aba
dahquang16 <23010230@st.phenikaa-uni.edu.vn>
2025-10-09 10:26:46 +0700

    Add files via upload

---
+++ src/main/OnlineBookStore/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.onlinebookstore.OnlineBookStore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("testuser")
+                .password(passwordEncoder().encode("123456"))
+                .roles("USER")
+            .and()
+            .withUser("admin")
+                .password(passwordEncoder().encode("admin123"))
+                .roles("ADMIN");
+    }
+
+@Override
+protected void configure(HttpSecurity http) throws Exception {
+    http
+        .authorizeRequests()
+            .antMatchers("/admin/**").hasRole("ADMIN")
+            .antMatchers("/user/**").hasRole("USER")
+            .antMatchers("/", "/registration", "/user-login", "/admin-login", "/login/admin").permitAll()
+            .antMatchers("/views/**", "/css/**", "/js/**", "/images/**").permitAll()
+            .anyRequest().authenticated()
+        .and()
+        .formLogin()
+            .loginPage("/admin-login")
+            .loginProcessingUrl("/login/admin")
+            .defaultSuccessUrl("/admin/home", true)
+            .permitAll()
+        .and()
+        .logout()
+            .permitAll()
+        .and()
+        .csrf().disable();
+}
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
