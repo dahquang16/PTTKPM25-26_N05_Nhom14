8859be41afdec32b6ecafa5ddaa6f373b7cf4b85
dahquang16 <23010230@st.phenikaa-uni.edu.vn>
2025-10-16 01:20:34 +0700

    Add files via upload

---
+++ backend/src/main/java/com/bookstore/dto/RegisterRequest.java
@@ -0,0 +1,77 @@
+package com.bookstore.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class RegisterRequest {
+    
+    @NotBlank
+    @Size(max = 50)
+    private String name;
+    
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+    
+    @NotBlank
+    @Size(max = 20)
+    private String phone;
+    
+    @NotBlank
+    @Size(min = 6, max = 100)
+    private String password;
+    
+    private String address;
+    
+    public RegisterRequest() {}
+    
+    public RegisterRequest(String name, String email, String phone, String password, String address) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.address = address;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getPhone() {
+        return phone;
+    }
+    
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
