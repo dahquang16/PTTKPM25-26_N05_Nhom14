a9e1fe13e86bef238ec498e75e0d75ffc2e0f8ff
chiduongkim <150871362+chiduongkim@users.noreply.github.com>
2025-10-16 09:29:46 +0700

    Create LoadingSpinner.js

---
+++ frontend/src/components/LoadingSpinner.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SpinnerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 4rem;
+  min-height: 200px;
+`;
+
+const Spinner = styled.div`
+  width: 50px;
+  height: 50px;
+  border: 4px solid #e5e7eb;
+  border-top: 4px solid #3b82f6;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+`;
+
+const LoadingSpinner = () => {
+  return (
+    <SpinnerContainer>
+      <Spinner />
+    </SpinnerContainer>
+  );
+};
+
+export default LoadingSpinner;
+
+
